f3ad8691aef9240ba3ec453085e7e44619467815
Prarambha Kumar <satwikmehta07@gmail.com>
2025-04-27 02:57:32 +0530

    new changes

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 // script.js
 
-window.addEventListener('load', () => {
-    const overlay = document.querySelector('.overlay');
-    overlay.addEventListener('animationend', () => {
-        overlay.style.display = 'none';
-    });
+// window.addEventListener('load', () => {
+//     const overlay = document.querySelector('.overlay');
+//     overlay.addEventListener('animationend', () => {
+//         overlay.style.display = 'none';
+//     });
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+        document.body.classList.add("loaded");
+    }, 1300); // Match the duration of the loader animation (1.7 seconds)
 });
+
 
 document.querySelector('.ui-button').addEventListener('click', () => {
     alert('UI Elements are shown!');
